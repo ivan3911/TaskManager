9cda04f076c85123ce8957efcf2cbd1941ebce4f
Demetrio Bautista <demet2824@gmail.com>
2025-12-17 10:29:07 -0600

    Se agrega interfaz y clase para uso de patron repository

---
+++ TaskManager/Data/Repository/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskManager.Data.Repository
+{
+    //interfaz para el patron repository
+
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<Entities.Task>> GetAllTasks();
+        Task<Entities.Task?> GetTaskById(int id);
+        Task AddTask(Entities.Task task);
+        Task UpdateTask(Entities.Task task);
+        Task DeleteTask(int id);
+    }
+}
---
+++ TaskManager/Data/Repository/TaskRepository.cs
@@ -0,0 +1,40 @@
+﻿
+namespace TaskManager.Data.Repository
+{
+
+    //implementacion de la interfaz para el patron repository
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public System.Threading.Tasks.Task AddTask(Entities.Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Threading.Tasks.Task DeleteTask(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Entities.Task>> GetAllTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Entities.Task?> GetTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Threading.Tasks.Task UpdateTask(Entities.Task task)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
