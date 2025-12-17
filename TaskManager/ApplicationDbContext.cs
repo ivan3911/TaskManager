70aecfabe1637830ef26fbe171706f97c94705d1
Demetrio Bautista <demet2824@gmail.com>
2025-12-17 10:14:55 -0600

    Configuracion para uso de DBContext con SQLServer

---
+++ TaskManager/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Entities.Task> Tasks { get; set; }
+
+    }
+}
