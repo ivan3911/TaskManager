092ad7b02ec90308974587de6d4b385a7be032a3
Demetrio Bautista <demet2824@gmail.com>
2025-12-18 12:46:13 -0600

    Se habilita Swagger

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -35,12 +35,9 @@ var app = builder.Build();
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 
